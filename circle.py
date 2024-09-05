d7a81e68b6ee446d34aba604c91e9fe0a9ef78c0
Parth Kesar <parthkesar@gmail.com>
2024-09-05 20:14:12 +0530

    Complete

--- circle.py
+++
@@ -1,7 +0,0 @@
-#Circle
-import math
-r = float(input("Enter the radius of circle: "))
-
-print("Area: ", math.pi*r*r)
-
-print("Perimeter: ", math.pi*2*r)
---
+++ rectangle.py
@@ -0,0 +1,6 @@
+#length
+l = int(input("Enter Length: "))
+b = int(input("Enter Breadth: "))
+
+print("Area: ", l*b)
+print("Perimeter: ", 2*(l+b))
